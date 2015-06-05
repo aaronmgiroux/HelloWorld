a37ae8dc3df2013d63c32756ca9d04d9c2b5bbb2
Aaron Giroux <aaronmgiroux@gmail.com>
2015-06-05 11:31:42 -0400

    gfhgfh
    gfhfghgfh

--- app/src/main/java/com/thyssenkrupp/helloworld/SecondActivity.java
+++ app/src/main/java/com/thyssenkrupp/helloworld/SecondActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.TextView;    
 
 public class SecondActivity extends ActionBarActivity {
 
